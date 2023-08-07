2dc908d8689f025b8899434e6c2dd8c0656bc4db
Nikita Malov <nikita.volam@gmail.com>
2023-08-07 12:26:10 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Spring Boot and Kafka demo
+
+- ✅ Simple event-driven microservices
+- ✅ Docker + Kafka and Spring Boot
+- ✅ 3 services to simulate simple real world scenario
+- ✅ Using one kafka topic for communication between order service and stock,email services
+
+Based on [Udemy course](https://www.udemy.com/course/building-microservices-with-spring-boot-and-spring-cloud/)
---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.example.orderservice.controller;
+
+import com.example.basedomains.dto.Order;
+import com.example.basedomains.dto.OrderEvent;
+import com.example.orderservice.kafka.OrderProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    @PostMapping("/orders")
+    public ResponseEntity<String> placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return ResponseEntity.ok("Order placed successfully");
+    }
+}
